63be68a08b3a3b3d58de713d2190f36f4d485e7c
Geldo Ronie Santos Silva <geldoronie@gmail.com>
2021-09-11 02:08:43 -0300

    Playfab implementations - Playfab Login - Playfab Register - Authentication Scene - Title Data Objects

---
+++ Assets/Scripts/GUI/AuthenticationGUI.cs
@@ -0,0 +1,130 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class AuthenticationGUI : MonoBehaviour
+{
+
+    [SerializeField]
+    private Authentication Authentication;
+    // Start is called before the first frame update
+
+    [SerializeField]
+    private Image LoginPanel;
+
+    [SerializeField]
+    private Image RegisterPanel;
+
+    [SerializeField]
+    private Image StartupPanel;
+
+    [SerializeField]
+    private Image LoadingPanel;
+
+    [Header("Login")]
+    [SerializeField]
+    private TMP_InputField UsernameField;
+
+    [SerializeField]
+    private TMP_InputField PasswordField;
+
+    [SerializeField]
+    private Button LoginButton;
+
+    [Header("Register")]
+    [SerializeField]
+    private TMP_InputField UsernameRegisterField;
+
+    [SerializeField]
+    private TMP_InputField PasswordRegisterField;
+
+    [SerializeField]
+    private TMP_InputField EmailRegisterField;
+
+    [SerializeField]
+    private TMP_InputField DisplayNameRegisterField;
+
+    [SerializeField]
+    private Button RegisterButton;
+
+    [Header("Startup")]
+    [SerializeField]
+    private Button RegisterScreenButton;
+
+    [SerializeField]
+    private Button LoginScreenButton;
+
+    private bool _waitForAuthenticationResponse = false;
+
+    void Awake()
+    {
+        this.LoginButton.onClick.AddListener(this.OnLoginButtonClick);  
+        this.RegisterButton.onClick.AddListener(this.OnRegisterButtonClick);
+        this.RegisterScreenButton.onClick.AddListener(this.OnRegisterScreenButtonClick);
+        this.LoginScreenButton.onClick.AddListener(this.OnLoginScreenButtonClick);
+        this.LoadingPanel.gameObject.SetActive(false);
+        this.OnBackToStartupScreenButtonClick();
+    }
+
+    void Update(){
+        if(!this._waitForAuthenticationResponse)
+            return;
+
+        if(!Authentication.Loading){
+            this.HideLoadingScreen();
+            this._waitForAuthenticationResponse = false;
+            if(Authentication.Registered || Authentication.Logged){
+                this.ClearInputs();
+            }
+        }
+    }
+
+    public void OnLoginButtonClick(){
+        Authentication.Login(this.UsernameField.text, this.PasswordField.text);
+        this.ShowLoadingScreen();
+        this._waitForAuthenticationResponse = true;
+    }
+
+    public void OnRegisterButtonClick(){
+        Authentication.Register(this.DisplayNameRegisterField.text, this.UsernameRegisterField.text, this.EmailRegisterField.text, this.PasswordRegisterField.text);
+        this.ShowLoadingScreen();
+        this._waitForAuthenticationResponse = true;
+    }
+
+    public void OnLoginScreenButtonClick(){
+        this.LoginPanel.gameObject.SetActive(true);
+        this.RegisterPanel.gameObject.SetActive(false);
+        this.StartupPanel.gameObject.SetActive(false);
+    }
+
+    public void OnRegisterScreenButtonClick(){
+        this.LoginPanel.gameObject.SetActive(false);
+        this.RegisterPanel.gameObject.SetActive(true);
+        this.StartupPanel.gameObject.SetActive(false);
+    }
+
+    public void OnBackToStartupScreenButtonClick(){
+        this.LoginPanel.gameObject.SetActive(false);
+        this.RegisterPanel.gameObject.SetActive(false);
+        this.StartupPanel.gameObject.SetActive(true);
+    }
+
+    public void ShowLoadingScreen(){
+        this.LoadingPanel.gameObject.SetActive(true);
+    }
+
+    public void HideLoadingScreen(){
+        this.LoadingPanel.gameObject.SetActive(false);
+    }
+
+    public void ClearInputs(){
+        this.UsernameField.text = "";
+        this.PasswordField.text = "";
+        this.DisplayNameRegisterField.text = "";
+        this.UsernameRegisterField.text = "";
+        this.EmailRegisterField.text = "";
+        this.PasswordRegisterField.text = "";
+    }
+}
---
+++ Assets/Scripts/Playfab/Authentication.cs
@@ -0,0 +1,116 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PlayFab;
+using PlayFab.ClientModels;
+
+public class Authentication : MonoBehaviour
+{
+
+    [SerializeField]
+    public string Username { get { return this._username; } }
+    [SerializeField]
+    public string Email { get { return this._email; } }
+    [SerializeField]
+    public string PlayFabId { get { return this._playFabId; } }
+    [SerializeField]
+    public bool Logged { get { return this._logged; } }
+    [SerializeField]
+    public bool Registered { get { return this._registered; } }
+    [SerializeField]
+    public string Ticket { get { return this._ticket; } }
+    [SerializeField]
+    public bool Loading { get { return this._loading; } }
+
+    #region Secrets     
+    const string TITLE_ID = "32052";
+
+    const string SECRET_KEY = "1MGQR6R3A87N7K6G5WWOBG5FY1BRPXG7GWONOC7179OIC5A7N7";
+
+    #endregion
+
+    private string _username;
+    private string _email;
+
+    private string _playFabId;
+
+    private string _ticket;
+
+    private bool _logged = false;
+
+    private bool _registered = false;
+
+    private bool _loading = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayFab.PlayFabSettings.TitleId = TITLE_ID;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Login(string username, string password){
+        var request = new LoginWithPlayFabRequest { 
+            Username = username,
+            Password = password,
+            TitleId = TITLE_ID
+        };
+
+        this._username = username;
+        this._loading = true;
+        this._logged = false;
+
+        PlayFabClientAPI.LoginWithPlayFab(request, this.OnLoginSuccess, this.OnLoginFailure);
+    }
+
+    public void Register(string displayName, string username, string email, string password){
+        var request = new RegisterPlayFabUserRequest { 
+            Username = username,
+            Password = password,
+            TitleId = TITLE_ID,
+            Email = email,
+            DisplayName = displayName
+        };
+
+        this._loading = true;
+        this._registered = false;
+
+        PlayFabClientAPI.RegisterPlayFabUser(request, this.OnRegisterSuccess, this.OnRegisterFailure);
+    }
+
+    private void OnLoginSuccess(LoginResult result){
+        this._email = "";
+        this._playFabId = result.PlayFabId;
+        this._ticket = result.SessionTicket;
+        this._logged = true;
+        this._loading = false;
+        Debug.Log("Logged with successful!");
+    }
+
+    private void OnLoginFailure(PlayFabError error){
+        this._logged = false;
+        this._username = "";
+        this._loading = false;
+        Debug.Log("Failed to loggin!");
+    }
+
+    private void OnRegisterSuccess(RegisterPlayFabUserResult result){
+        this._registered = true;
+        this._loading = false;
+        Debug.Log("Registered with successful!");
+    }
+
+    private void OnRegisterFailure(PlayFabError error){
+        this._logged = false;
+        this._registered = false;
+        this._username = "";
+        this._loading = false;
+        Debug.Log("Failed to register!");
+        Debug.Log(error);
+    }
+}
