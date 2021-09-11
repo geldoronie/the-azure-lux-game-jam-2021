63f2aae4e1714c82d243cc7b1153eb6abdb5fbc4
Guilherme Torres <gousenfire@gmail.com>
2021-09-11 13:36:45 -0300

    Added camera movement

--- Assets/Scripts/Game/CameraController.cs
+++ Assets/Scripts/Game/CameraController.cs
@@ -1,9 +1,11 @@
 using UnityEngine;
 
-public class CameraRotation : MonoBehaviour
+public class CameraController : MonoBehaviour
 {
+    [SerializeField] private Vector2 mapSize = new Vector2(20, 20);
     [SerializeField] private float _mouseSensitivity = 20f;
-    [SerializeField] private float _zoomSpeed = 120f;
+    [SerializeField] private float _movementSpeed = 10f;
+    [SerializeField] private float _zoomSpeed = 5f;
     [SerializeField] private float _yMinLimit = 0f;
     [SerializeField] private float _yMaxLimit = 80f;
     [SerializeField] private float _distanceMin = 10f;
@@ -11,11 +13,24 @@ public class CameraRotation : MonoBehaviour
 
     private float _currentDistance = 15f;
     private Vector2 _currentRotation;
+    private Vector3 _targetPosition;
 
     private void Start()
     {
         Vector3 angles = transform.eulerAngles;
         _currentRotation = new Vector2(angles.y, angles.x);
+    }
+
+    private void Update()
+    {
+        Vector3 position = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+        position = transform.right * position.x + transform.forward * position.z;
+        position.y = 0;
+        position.Normalize();
+
+        _targetPosition = Vector3.MoveTowards(_targetPosition, _targetPosition + position, _movementSpeed * Time.deltaTime);
+        _targetPosition.x = Mathf.Clamp(_targetPosition.x, -mapSize.x / 2, mapSize.x / 2);
+        _targetPosition.z = Mathf.Clamp(_targetPosition.z, -mapSize.y / 2, mapSize.y / 2);
     }
 
     private void LateUpdate()
@@ -31,7 +46,7 @@ public class CameraRotation : MonoBehaviour
         _currentDistance = Mathf.Clamp(_currentDistance - Input.GetAxis("Mouse ScrollWheel") * _zoomSpeed, _distanceMin, _distanceMax);
 
         Vector3 negDistance = new Vector3(0.0f, 0.0f, -_currentDistance);
-        Vector3 position = rotation * negDistance;
+        Vector3 position = rotation * negDistance + _targetPosition;
 
         transform.rotation = rotation;
         transform.position = position;
