927f140f02654ac00c99ff39e81be4246b619253
Guilherme Torres <gousenfire@gmail.com>
2021-09-11 11:23:27 -0300

    Small tweaks

--- Assets/Scripts/Game/CameraRotation.cs
+++ Assets/Scripts/Game/CameraRotation.cs
@@ -2,8 +2,8 @@ using UnityEngine;
 
 public class CameraRotation : MonoBehaviour
 {
-    [SerializeField] private float _xSpeed = 20f;
-    [SerializeField] private float _ySpeed = 120f;
+    [SerializeField] private float _mouseSensitivity = 20f;
+    [SerializeField] private float _zoomSpeed = 120f;
     [SerializeField] private float _yMinLimit = 0f;
     [SerializeField] private float _yMaxLimit = 80f;
     [SerializeField] private float _distanceMin = 10f;
@@ -22,13 +22,13 @@ public class CameraRotation : MonoBehaviour
     {
         if (Input.GetMouseButton(2))
         {
-            _currentRotation.x += Input.GetAxis("Mouse X") * _xSpeed * _currentDistance * Time.deltaTime;
-            _currentRotation.y -= Input.GetAxis("Mouse Y") * _ySpeed * Time.deltaTime;
+            _currentRotation.x += Input.GetAxis("Mouse X") * _mouseSensitivity * Time.deltaTime;
+            _currentRotation.y -= Input.GetAxis("Mouse Y") * _mouseSensitivity * Time.deltaTime * (Screen.width / Screen.height);
         }
 
         Quaternion rotation = Quaternion.Euler(ClampAngle(_currentRotation.y, _yMinLimit, _yMaxLimit), _currentRotation.x, 0);
 
-        _currentDistance = Mathf.Clamp(_currentDistance - Input.GetAxis("Mouse ScrollWheel") * 5, _distanceMin, _distanceMax);
+        _currentDistance = Mathf.Clamp(_currentDistance - Input.GetAxis("Mouse ScrollWheel") * _zoomSpeed, _distanceMin, _distanceMax);
 
         Vector3 negDistance = new Vector3(0.0f, 0.0f, -_currentDistance);
         Vector3 position = rotation * negDistance;
