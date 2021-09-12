8409bb35c7d4b1e9cefc60dcb1203bb9f0d2a327
Geldo Ronie Santos Silva <geldoronie@gmail.com>
2021-09-12 10:34:45 -0300

    Scene GUI elements - Fixing Scene WorldVsYou - Phase Anouncement GUI

--- Assets/Scripts/Game/GameModeWorldVsYou.cs
+++ Assets/Scripts/Game/GameModeWorldVsYou.cs
@@ -26,21 +26,75 @@ public class GameModeWorldVsYou : GameModeBase
     }
 
     private void _playerTurnUpdate(){
-        if(this.Timer.Remaining <= 0){
-            this.ChangeTurn();
-            this.Timer.ResetTimer();
-            this.Timer.StartTimer();
-            
+        if(this.CurrentTurnPhase == TurnPhase.Main){
+            if(this.Timer.Remaining <= 0){
+                this.ChangePhase(TurnPhase.Refill);
+                this.Timer.StartTime = 2;
+                this.Timer.ResetTimer();
+                this.Timer.StartTimer();
+            }
+        } else if(this.CurrentTurnPhase == TurnPhase.Refill){
+            if(this.Timer.Remaining <= 0){
+                this.GivePlayerBaseResources();
+                this.ChangePhase(TurnPhase.Draw);
+                this.Timer.StartTime = 2;
+                this.Timer.ResetTimer();
+                this.Timer.StartTimer();
+            }
+        } else if(this.CurrentTurnPhase == TurnPhase.Draw){
+            if(this.Timer.Remaining <= 0){
+                this.Player.DrawCard(1);
+                this.Timer.StartTime = this.PlayTurnTime;
+                this.Timer.ResetTimer();
+                this.Timer.StartTimer();
+                this.ChangePhase(TurnPhase.Play);
+            }
+        } else if(this.CurrentTurnPhase == TurnPhase.Play){
+            if(this.Timer.Remaining <= 0){
+                this.ChangeTurn();
+                this.Timer.StartTime = 4;
+                this.Timer.ResetTimer();
+                this.Timer.StartTimer();
+            }
         }
     }
 
     private void _cpuTurnUpdate(){
-        if(this.Timer.Remaining <= 0){
-            this.ChangeTurn();
-            this.Timer.ResetTimer();
-            this.Timer.StartTimer();
-            //Give PlayerBaseResources
-            this.GivePlayerBaseResources();
+        if(this.CurrentTurnPhase == TurnPhase.Main){
+            if(this.Timer.Remaining <= 0){
+                this.ChangePhase(TurnPhase.Destroy);
+                this.Timer.StartTime = 2;
+                this.Timer.ResetTimer();
+                this.Timer.StartTimer();
+            }
+        } else if(this.CurrentTurnPhase == TurnPhase.Destroy){
+            if(this.Timer.Remaining <= 0){
+                this.ChangePhase(TurnPhase.Disaster);
+                this.Timer.StartTime = 2;
+                this.Timer.ResetTimer();
+                this.Timer.StartTimer();
+            }
+        } else if(this.CurrentTurnPhase == TurnPhase.Disaster){
+            if(this.Timer.Remaining <= 0){
+                this.ChangePhase(TurnPhase.Military);
+                this.Timer.StartTime = 2;
+                this.Timer.ResetTimer();
+                this.Timer.StartTimer();
+            }
+        } else if(this.CurrentTurnPhase == TurnPhase.Military){
+            if(this.Timer.Remaining <= 0){
+                this.Timer.StartTime = 10;
+                this.Timer.ResetTimer();
+                this.Timer.StartTimer();
+                this.ChangePhase(TurnPhase.Play);
+            }
+        } else if(this.CurrentTurnPhase == TurnPhase.Play){
+            if(this.Timer.Remaining <= 0){
+                this.ChangeTurn();
+                this.Timer.StartTime = 4;
+                this.Timer.ResetTimer();
+                this.Timer.StartTimer();
+            }
         }
     }
 
