10620c1a0e9d6cb417df0415908c5a5989cfae1f
Geldo Ronie Santos Silva <geldoronie@gmail.com>
2021-09-12 22:43:30 -0300

    Scenes, GameMode, Player Statistics,Player Profile - Fixing Player Statistics Callbacks - Fixing Player Profile Callbacks - Creating Authentication Callbacks - New Main Menu Scene - New End Game Summary Scene

---
+++ Assets/Scripts/Game/PlayerProfile.cs
@@ -0,0 +1,5 @@
+public class PlayerProfile
+{
+    public string displayName;
+    public string avatarUrl;
+}
---
+++ Assets/Scripts/Playfab/PlayerStatsPlayfab.cs
@@ -0,0 +1,97 @@
+using System.Collections.Generic;
+using UnityEngine;
+using PlayFab;
+using PlayFab.ClientModels;
+using UnityEngine.Events;
+
+public struct Matches
+{
+    public List<GameStats> games;
+}
+
+public class PlayerStatsPlayfab : MonoBehaviour
+{
+    [SerializeField] private AuthenticationPlayfab Authentication;
+
+    [SerializeField] private List<GameStats> Matches;
+
+    [SerializeField] private GameStats LastMatchStats;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+        this.Authentication = GameObject.Find("AuthenticationPlayfab").GetComponent<AuthenticationPlayfab>();
+        this.Authentication.OnAuthenticationSuccess += LoadPlayerStats;
+    }
+
+    public void SaveMatchStatistic(GameStats gameStats){
+        this.LastMatchStats = gameStats;
+        this.Matches.Add(gameStats);
+        var matches = new Matches(){
+            games = this.Matches
+        };
+        this._setMatchesStatistics(matches);
+    }
+
+    public void LoadPlayerStats(){
+        this.Authentication.OnAuthenticationSuccess -= LoadPlayerStats;
+        this._getMatchesStatistics();
+    }
+
+    private void _setMatchesStatistics(Matches matches)
+    {
+        Dictionary<string, string> matchesData = new Dictionary<string, string>();
+        matchesData.Add(
+            "matches",
+            JsonUtility.ToJson(matches)
+        );
+        var request = new UpdateUserDataRequest
+        {
+            Data = matchesData,
+            AuthenticationContext = this.Authentication.AuthenticationContext
+        };
+
+        PlayFabClientAPI.UpdateUserData(request, this._onSetMatchesStatisticsSuccess, this._onSetMatchesStatisticsFailure);
+    }
+
+    private void _getMatchesStatistics()
+    {
+        var request = new GetUserDataRequest
+        {
+            Keys = new List<string> {
+                "matches"
+            },
+            AuthenticationContext = this.Authentication.AuthenticationContext
+        };
+
+        PlayFabClientAPI.GetUserData(request, this._onGetMatchesStatisticsSuccess, this._onGetMatchesStatisticsFailure);
+    }
+
+    private void _onGetMatchesStatisticsSuccess(GetUserDataResult result)
+    {
+        if (!result.Data.ContainsKey("matches"))
+        {
+            Debug.Log("There's no 'matches statistics' key at PlayerDataTitle in Playfab Configuration");
+            return;
+        }
+
+        this.Matches = JsonUtility.FromJson<Matches>(result.Data["matches"].Value).games;
+
+        Debug.Log("Match Statistics Loaded!");
+    }
+
+    private void _onGetMatchesStatisticsFailure(PlayFabError error)
+    {
+        Debug.LogError(error);
+    }
+    
+    private void _onSetMatchesStatisticsSuccess(UpdateUserDataResult result)
+    {
+        Debug.Log("Match Statistics Saved!");
+    }
+
+    private void _onSetMatchesStatisticsFailure(PlayFabError error)
+    {
+        Debug.LogError(error);
+    }
+}
